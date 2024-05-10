addd5109d26a60af281ca7dc15801ec8e6ef1e12
jeanhebert <jeanheberth19@gmail.com>
2024-05-10 11:38:49 -0300

    Initial commit

---
+++ src/main/java/com/br/empresa/api/EmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.br.empresa.api.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpresaApplication.class, args);
+    }
+
+}
