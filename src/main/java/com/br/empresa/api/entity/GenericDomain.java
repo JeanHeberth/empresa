f78ff4654a96d2e8a0eccf697a608726b4e0c464
jeanhebert <jeanheberth19@gmail.com>
2024-05-10 13:12:24 -0300

    foi corrigido o arquivo para conexão do banco de dados. E iniciado a criação das classes

---
+++ src/main/java/com/br/empresa/api/entity/GenericDomain.java
@@ -0,0 +1,22 @@
+package com.br.empresa.api.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@SuppressWarnings("serial")
+public class GenericDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
---
+++ src/main/java/com/br/empresa/api/repository/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package com.br.empresa.api.repository;
+
+import com.br.empresa.api.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    Optional<Funcionario> findByEmail(String email);
+}
