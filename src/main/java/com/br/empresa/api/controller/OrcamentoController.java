3e050a94410ed4daeaa9940a7d5085e76de6ac0f
jeanhebert <jeanheberth19@gmail.com>
2024-06-05 10:40:53 -0300

    Criado o método de buscar o orcamento por id.

--- src/main/java/com/br/empresa/api/controller/OrcamentoController.java
+++ src/main/java/com/br/empresa/api/controller/OrcamentoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/orcamentos")
+@RequestMapping("/api/orcamento")
 public class OrcamentoController {
 
     @Autowired
@@ -20,6 +20,11 @@ public class OrcamentoController {
     @GetMapping
     public ResponseEntity<List<OrcamentoResponseDto>> buscarTodosOrcamento() {
         return ResponseEntity.ok(orcamentoService.buscarOrcamentos());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<OrcamentoResponseDto> buscarOrcamentoPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(orcamentoService.buscarOrcamentoPorId(id));
     }
 
     @PostMapping
